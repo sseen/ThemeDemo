ea6acde16569db68859701701afbd50da613a403
sseen <ssnfee@gmail.com>
2017-08-02 17:21:26 +0800

    tint color not enable label

--- ThemeDemo/ViewController.swift
+++ ThemeDemo/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    @IBOutlet weak var lblOne: UILabel!
+    @IBOutlet weak var lblTwo: UILabel!
+    @IBOutlet weak var tfOne: UITextField!
+    @IBOutlet weak var tfTwo: UITextField!
+    
+    let oneColor = UIColor.purple
+    let twoColor = UIColor(red:0.99, green:0.69, blue:0.31, alpha:1.00)
+    let othColor = UIColor(red:0.92, green:0.42, blue:0.37, alpha:1.00)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tfOne.text = "one"
+        self.tfTwo.text = "two"
+        self.title = "title"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ckOne(_ sender: UIButton) {
+        lblOne.backgroundColor = oneColor
+        lblOne.tintColor = twoColor
+        lblOne.textColor = othColor
+        
+        sender.backgroundColor = oneColor
+        sender.tintColor = twoColor
+        
+        self.navigationController?.navigationBar.tintColor = twoColor
+    }
 
+    @IBAction func ckTwo(_ sender: Any) {
+    }
 }
 
